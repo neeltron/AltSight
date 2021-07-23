e9f0e3f40358b24c924993fea14dd307373b20ca
Neel Adwani <nprosofficial@gmail.com>
2021-07-23 22:03:49 +0000

    added google's object detection

--- googlecloudtest.py
+++ googlecloudtest.py
@@ -1,2 +1,13 @@
 import os
 import io
+
+def detect(img):
+  from google.cloud import vision
+  client = vision.ImageAnnotatorClient()
+  file_name = os.path.abspath(img)
+  with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+  image = vision.Image(content=content)
+  response = client.label_detection(image=image)
+  labels = response.label_annotations
+  return labels
