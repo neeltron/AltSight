ff0dba694081b9882d3ee660da943a4bd32717c2
Neel Adwani <nprosofficial@gmail.com>
2021-07-24 10:11:38 +0000

    added infinite loop

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 import io
 from picamera import PiCamera
+from time import sleep
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Downloads/cognitio-7378d-f0c72d841d75.json"
-
-camera = PiCamera()
-camera.capture('live.jpg')
 
 def detect(img):
   from google.cloud import vision
@@ -18,15 +16,18 @@ def detect(img):
   labels = response.label_annotations
   return labels
 
-labels = detect('live.jpg')
-print(labels)
 
-temp = 0
-desc = ""
+while True:
+  camera = PiCamera()
+  camera.capture('live.jpg')
+  labels = detect('live.jpg')
+  temp = 0
+  desc = ""
 
-for i in labels:
-  print(i.description)
-  if temp == 0:
-    desc = i.description
-  temp += 1
-os.system('espeak "' + desc + '"')
+  for i in labels:
+    print(i.description)
+    if temp == 0:
+      desc = i.description
+    temp += 1
+  os.system('espeak "' + desc + '"')
+  sleep(2)
